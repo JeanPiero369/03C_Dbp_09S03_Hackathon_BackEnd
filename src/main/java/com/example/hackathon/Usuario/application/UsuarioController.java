39bc95fcd27cd60f6d165397bc358b24e7bf3507
Ian Condori Flores <ian@MacBook-Pro-de-Cielo.local>
2024-06-01 15:51:53 -0500

    cancion controller done

---
+++ src/main/java/com/example/hackathon/Usuario/application/UsuarioController.java
@@ -0,0 +1,12 @@
+package com.example.hackathon.Usuario.application;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+    @GetMapping("/users/{user_id}/playlists")
+    public ResponseEntity<>
+
+}
