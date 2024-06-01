18a05d692b46d047b7afc4f0908ebd66ccaea421
Ian Condori Flores <ian@MacBook-Pro-de-Cielo.local>
2024-06-01 16:02:23 -0500

    done some

--- src/main/java/com/example/hackathon/Usuario/domain/UsuarioService.java
+++ src/main/java/com/example/hackathon/Usuario/domain/UsuarioService.java
@@ -1,5 +1,8 @@
 package com.example.hackathon.Usuario.domain;
 
+import com.example.hackathon.Exceptions.NotFoundException;
+import com.example.hackathon.ListaDeReproduccion.domain.ListaDeReproduccion;
+import com.example.hackathon.ListaDeReproduccion.infrastructure.ListaDeReproduccionRepository;
 import com.example.hackathon.Usuario.infrastructure.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,12 +10,16 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class UsuarioService implements UserDetailsService {
     @Autowired
-    private UsuarioRepository usuarioRepository;
+    private UsuarioRepository<Usuario> usuarioRepository;
+
+    @Autowired
+    ListaDeReproduccionRepository listaDeReproduccionRepository;
 
     public UserDetailsService userDetailsService() {
         return username -> {
@@ -26,4 +33,21 @@ public class UsuarioService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return (Usuario) usuarioRepository.findByEmail(username).get();
     }
+
+    public List<ListaDeReproduccion> getAllPlaylists(Long id) {
+        Usuario usuario = usuarioRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("User not found")
+        );
+
+        return listaDeReproduccionRepository.findAllByUsuarioId(id);
+    }
+
+    public ListaDeReproduccion getPlaylist(Long id) {
+        ListaDeReproduccion lista =  listaDeReproduccionRepository.findById(id).orElseThrow(
+                () -> new NotFoundException("Playlist not found")
+        );
+        return lista;
+    }
+
+    public
 }
