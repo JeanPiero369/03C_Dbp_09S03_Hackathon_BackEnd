0472641faf78c6273d238210f00ef8c0a67260e9
Ian Condori Flores <ian@MacBook-Pro-de-Cielo.local>
2024-06-01 15:48:25 -0500

    merged main

--- src/main/java/com/example/hackathon/Cancion/infrastructure/CancionRepository.java
+++ src/main/java/com/example/hackathon/Cancion/infrastructure/CancionRepository.java
@@ -4,6 +4,9 @@ import com.example.hackathon.Cancion.domain.Cancion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CancionRepository extends JpaRepository<Cancion, Long> {
+    Optional<Cancion> findByTituloAndArtistaId(String titulo, Long artistaId);
 }
